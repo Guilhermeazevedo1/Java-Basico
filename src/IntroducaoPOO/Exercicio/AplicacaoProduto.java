23a6cb4fd2d58bc00c682552feb57100fed41493
Guilhermeazevedo1 <guilhermeazevedo_2603@hotmail.com>
2025-06-23 19:16:06 -0300

    atualizacao

--- src/IntroducaoPOO/Exercicio/AplicacaoProduto.java
+++ src/IntroducaoPOO/Exercicio/AplicacaoProduto.java
@@ -1,4 +1,39 @@
 package IntroducaoPOO.Exercicio;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AplicacaoProduto {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+
+        EntitiesProduto produto = new EntitiesProduto();
+        System.out.println("Digite os dados do produto");
+        System.out.print("Name: ");
+        produto.name = sc.next();
+        System.out.print("Preço do produto: ");
+        produto.price = sc.nextDouble();
+        System.out.print("Quantidade do produto: ");
+        produto.quantidade = sc.nextInt();
+
+        System.out.printf("O produto %s tem o preço %.2f e a quantidade %d%n%n", produto.name, produto.price, produto.quantidade);
+
+        System.out.printf("Qual o valor do estoque de %s: %n", produto.name);
+        System.out.println(produto.valorTotalEstoque());
+
+        System.out.printf("Digite quantos %s deseja remover: ", produto.name);
+        int qntRemovido = sc.nextInt();
+        produto.removerProtudos(qntRemovido);
+        System.out.println("A quantidade de produtos agora é: " + produto.quantidade);
+
+        System.out.printf("Digite quantos(as) %s entrou: ", produto.name);
+        int produtoAdd = sc.nextInt();
+        produto.addProtudos(produtoAdd);
+        System.out.println("A quantidade de produtos agora é: " + produto.quantidade);
+
+        System.out.println("O valor final do estoque é: " + produto.valorTotalEstoque());
+
+        sc.close();
+    }
 }
--- src/IntroducaoPOO/Exercicio/EntitiesProduto.java
+++ src/IntroducaoPOO/Exercicio/EntitiesProduto.java
@@ -1,4 +1,19 @@
 package IntroducaoPOO.Exercicio;
 
 public class EntitiesProduto {
+    public String name;
+    public double price;
+    public int quantidade;
+
+    public double valorTotalEstoque(){
+        return price * quantidade;
+    }
+
+    public void addProtudos(int quantidade){
+        this.quantidade += quantidade;
+    }
+
+    public void removerProtudos(int quantidade){
+        this.quantidade -= quantidade;
+    }
 }

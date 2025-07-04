e64cffd146d2ddf7126d02327940fa903d48c1d2
Guilhermeazevedo1 <guilhermeazevedo_2603@hotmail.com>
2025-07-03 23:33:22 -0300

    atualização do exercicio de fiexação matriz

--- src/Matriz/ExercicioFixacao.java
+++ src/Matriz/ExercicioFixacao.java
@@ -6,10 +6,38 @@ public class ExercicioFixacao {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Escolha o tamanho das colunas");
-        int n = sc.nextInt();
-        System.out.println("Escolha o tamanho das linhas");
+        System.out.println("Escolha o tamanho das colunas: ");
         int m = sc.nextInt();
+        System.out.println("Escolha o tamanho das linhas: ");
+        int n = sc.nextInt();
+        int[][] mat = new int[m][n];
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat[i].length; j++){
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        int numeroEscolhido = sc.nextInt();
+
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat[i].length; j++){
+                if(mat[i][j] == numeroEscolhido){
+                    System.out.println("Posição do número na matriz é: " + i + ", " + j);
+                    if(j > 0){
+                        System.out.println("Número a esquerda: " + mat[i][j-1]);
+                    }
+                    if(i > 0){
+                        System.out.println("Número acima: " + mat[i-1][j]);
+                    }
+                    if(j < mat[i].length - 1){
+                        System.out.println("Número a direita: " + mat[i][j+1]);
+                    }
+                    if(i < mat.length - 1){
+                        System.out.println("Número abaixo: " + mat[i+1][j]);
+                    }
+                }
+            }
+        }
 
         sc.close();
     }

d3095cc15d9ccbb8f7adea5b2a31ca78d23fde11
Guilhermeazevedo1 <guilhermeazevedo_2603@hotmail.com>
2025-07-13 22:07:58 -0300

    try catch basico

--- src/TratamentoExcecoes/Programa.java
+++ src/TratamentoExcecoes/Programa.java
@@ -1,7 +1,24 @@
 package TratamentoExcecoes;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Programa {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            String[] vetor = sc.nextLine().split(" ");
+            int posicao = sc.nextInt();
+            System.out.println(vetor[posicao]);
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Posição inválida");
+        }
+        catch (InputMismatchException e){
+            System.out.println("Valor digitado não corresponde a um número");
+        }
 
+        System.out.println("Fim do programa!");
+        sc.close();
     }
 }

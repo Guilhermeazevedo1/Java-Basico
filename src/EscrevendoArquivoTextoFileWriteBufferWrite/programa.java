fdf456424c96b00b2d8cd0d3ddac40b31832f289
Guilhermeazevedo1 <guilhermeazevedo_2603@hotmail.com>
2025-08-22 23:58:23 -0300

    criação de novas pastas assunto files

---
+++ src/EscrevendoArquivoTextoFileWriteBufferWrite/programa.java
@@ -0,0 +1,23 @@
+package EscrevendoArquivoTextoFileWriteBufferWrite;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class programa {
+    public static void main(String[] args) {
+        String[] lines = new String[]{"Good morning", "Good afternoon", "Good night"};
+
+        String path = "C:\\Users\\Guilherme\\Documents\\out.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+            for (String line : lines){
+                bw.write(line);
+                bw.newLine();
+            }
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}

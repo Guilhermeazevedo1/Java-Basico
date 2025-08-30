9b80f8a79ca1752ba7480540eec0e81909ba1725
Guilhermeazevedo1 <guilhermeazevedo_2603@hotmail.com>
2025-08-30 08:12:41 -0300

    criação de novas pastas

---
+++ src/Map/application/Program.java
@@ -0,0 +1,29 @@
+package Map.application;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Map<String, String> cookies = new TreeMap<>();
+
+        cookies.put("username", "Maria");
+        cookies.put("email", "maria@gmail.com");
+        cookies.put("phone", "9999999999");
+
+        cookies.remove("email");
+        cookies.put("phone", "88888888888");
+
+        System.out.println("Contains 'phone' key: " + cookies.containsKey("phone"));
+        System.out.println("Phone number: " + cookies.get("phone"));
+        System.out.println("Email: " + cookies.get("email"));
+        System.out.println("Size: " + cookies.size() );
+
+        System.out.println("All Cookies: ");
+        for (String key : cookies.keySet()){
+            System.out.println(key + ": " + cookies.get(key ));
+        }
+    }
+
+}
